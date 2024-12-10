0ad7147168c0f0a575984f48d291c6755067e415
ElnarMagdiev <elnarmag263@gmail.com>
2024-12-10 18:37:29 +0300

    9_0_unused_import commit

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -2,25 +2,22 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 
-import java.net.URI;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = MealRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
-public class MealRestController extends AbstractMealController {
-    static final String REST_URL = "/rest/profile/meals";
+@RequestMapping(value = "/profile/meals", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MealUIController extends AbstractMealController {
 
     @Override
-    @GetMapping("/{id}")
+    @GetMapping("/id")
     public Meal get(@PathVariable int id) {
         return super.get(id);
     }
@@ -38,22 +35,13 @@ public class MealRestController extends AbstractMealController {
         return super.getAll();
     }
 
-    @Override
-    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@RequestBody Meal meal, @PathVariable int id) {
-        super.update(meal, id);
-    }
-
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Meal> createWithLocation(@RequestBody Meal meal) {
-        Meal created = super.create(meal);
-
-        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path(REST_URL + "/{id}")
-                .buildAndExpand(created.getId()).toUri();
-
-        return ResponseEntity.created(uriOfNewResource).body(created);
+    public void updateOrCreate(
+            @RequestParam LocalDateTime dateTime,
+            @RequestParam String description,
+            @RequestParam int calories) {
+        super.create(new Meal(dateTime, description, calories));
     }
 
     @Override
